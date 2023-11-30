da35c6959b7540624fc4fad5dfe11f11b718df30
MariaStepanyan <drmariastepanyan@gmail.com>
2023-11-30 20:05:53 +0300

    div photo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './App.css'
 import { Box } from './components/box.styled'
 import { StyledCard } from './components/Card.styled'
+import { PhotoCard } from './components/photoCard/PhotoCard'
 
 function App() {
   return (
     <div>
       <Box>
-        <StyledCard>hello</StyledCard>
+        <StyledCard>
+          <PhotoCard aria-label="Card photo" />
+        </StyledCard>
       </Box>
     </div>
   )
---
+++ src/components/photoCard/PhotoCard.tsx
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components'
+import PhotoCardImg from './photoCard.png'
+
+export const PhotoCard = styled.div`
+  width: 280px;
+  height: 170px;
+  margin: 10px; 
+border-radius: 10px;
+background-image: url(${PhotoCardImg});
+`
